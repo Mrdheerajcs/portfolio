dac53890ed53a367b24d84f37c6f9449ea36ab33
arvindrajcs <arvindrajcs@gmail.com>
2025-01-02 16:57:34 +0530

    war

--- src/main/java/com/auspicius/PortfoliosApplication.java
+++ src/main/java/com/auspicius/PortfoliosApplication.java
@@ -2,12 +2,18 @@ package com.auspicius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortfoliosApplication {
+public class PortfoliosApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfoliosApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PortfoliosApplication.class);
+    }
 }
